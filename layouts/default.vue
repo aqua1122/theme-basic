df37c499b69b2af5e0ed3d1e6fabdf1cc53a3890
AQUA <aqua1245@proton.me>
2025-06-21 02:44:56 +0800

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,7 @@ const head = useLocaleHead({
 
 useHead({
   meta: [
-    { 'name': 'revisit-after', 'content': '1 days' },
+    { name: 'revisit-after', content: '1 days' },
   ],
 })
 
@@ -17,23 +17,32 @@ useSeoMeta({
 })
 </script>
 
+<style>
+html, body {
+  background: #1a110c !important; /* 진한 브라운 */
+  color: #d4af37 !important;      /* 은은한 골드 텍스트 */
+  min-height: 100vh;
+  margin: 0;
+}
+</style>
+
 <template>
   <div>
     <Html :lang="head.htmlAttrs.lang" :dir="head.htmlAttrs.dir">
 
-    <Head>
-      <template v-for="link in head.link" :key="link.id">
-        <Link :id="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
-      </template>
-      <template v-for="meta in head.meta" :key="meta.id">
-        <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
-      </template>
-    </Head>
+      <Head>
+        <template v-for="link in head.link" :key="link.id">
+          <Link :id="link.id" :rel="link.rel" :href="link.href" :hreflang="link.hreflang" />
+        </template>
+        <template v-for="meta in head.meta" :key="meta.id">
+          <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
+        </template>
+      </Head>
 
-    <Body>
-      <slot />
-      <Alert />
-    </Body>
+      <Body>
+        <slot />
+        <Alert />
+      </Body>
 
     </Html>
   </div>
